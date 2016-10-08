309aea3a65a8f26213bce982323cbbffdcdf07da
ResVR <resvr@media.mit.edu>
2016-10-08 15:48:35 -0400

    Initial VR interaction and ball drop
    Also added time scaling

--- Assets/Scripts/Mechanics.cs
+++ Assets/Scripts/Mechanics.cs
@@ -2,46 +2,33 @@
 using System.Collections;
 
 public class Mechanics : MonoBehaviour {
-	private Vector3 velocity = new Vector3 (0, 0, 0);
+	public Vector3 velocity = new Vector3 (0, 0, 0);
 
-	public bool enableMotion = false;
-
+	public bool enableMotion = true;
 
 	// Use this for initialization
 	void Start () {
-
-		velocity.x = 5f;
-		velocity.y = 10f;
-
+		
 	}
 
-	void reset(string Test) {
-		Debug.Log ("RESET");
-		velocity.x = 5f;
-		velocity.y = 10f;
-		this.transform.localPosition = Vector3.zero;
-		Debug.Log (Test);
-	
-	}
 
 	// Update is called once per frame
 	void Update () {
-		Debug.Log (this.transform.position.y);
 
-
-		if (Input.GetKeyDown("space"))
-			enableMotion = !enableMotion;
-
-		if (Input.GetKeyDown ("r")) {
-			reset ("test");
-		}
 
 		if (enableMotion) {
 			// v = v0 + at
-			velocity.y += Time.deltaTime * -9.8f;
+			velocity.y += Time.deltaTime * Globals.timeScale * -9.8f;
 
-
-			this.transform.Translate (velocity * Time.deltaTime);
+			if (this.transform.localPosition.y <= 0) {
+				velocity.y = 0;
+				this.transform.localPosition = 
+					new Vector3 (this.transform.localPosition.x, 0, this.transform.localPosition.z);
+			}
+			else {
+				this.transform.Translate (velocity * Time.deltaTime * Globals.timeScale);
+			}
 		}
+
 	}
 }
