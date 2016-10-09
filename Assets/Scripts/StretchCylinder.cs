4bbd39d994400709814370133917a8d1d253eec9
Chris <christopher19465@gmail.com>
2016-10-09 14:03:05 -0400

    added arrows

---
+++ Assets/Scripts/StretchCylinder.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StretchCylinder : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // TODO remove this transformation!
+        this.transform.Translate(new Vector3(0, .01f, 0));
+
+        Vector3 position = this.transform.localPosition;
+
+        // based on position, stretch cylinder
+        GameObject xCyl = GameObject.Find("StretchCylinderX");
+        xCyl.transform.localScale = new Vector3(.2f, .2f+position.y, .2f);
+        Debug.Log(xCyl.transform.localScale);
+    }
+}
---
+++ Assets/Scripts/StretchCylinderY.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StretchCylinderY : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // TODO remove this transformation!
+        this.transform.Translate(new Vector3(0, .005f, 0));
+
+        Vector3 position = this.transform.localPosition;
+
+        // based on position, stretch cylinder
+        GameObject yCyl = GameObject.Find("StretchCylinderY");
+        yCyl.transform.localScale = new Vector3(.2f, .2f+position.y, .2f);
+        Debug.Log(yCyl.transform.localScale);
+    }
+}
